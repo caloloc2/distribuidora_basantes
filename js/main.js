eb9b92dcffd89018290b081a1d8709221a1abc78
caloloc2 <calolomino@gmail.com>
2017-01-18 14:30:01 -0500

    actualiza

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $('document').ready(function(){
 	$('#cargando').fadeOut(250);
-	$('.slider').jcider({
-		autoplay: true
+	$('#slider').jcider({
+		looping: true, // For looping
+		visibleSlides: 1, // Visible no. of slides
+		variableWidth: false, // For variable width
+		variableHeight: true, // For variable height
+		fading: false, // For fading/sliding effect
+		easing: 'cubic-bezier(.694, .0482, .335, 1)', // For easing
+		transitionDuration: 400, // Duration of slide transition
+		autoplay: false, // Duh...
+		slideDuration: 3000, // Duration between each slide change in autoplay
+		controls: true, // For visibility of nav-arrows
+		controlsWrapper: 'div.jcider-nav', // Element for nav wrapper
+		controlsLeft: ['span.jcider-nav-left', ''], // Element for nav-left 
+		controlsRight: ['span.jcider-nav-right', ''], // Element for nav-right
+		pagination: true, // For visibility of pagination
+		paginationWrapper: 'div.jcider-pagination', // Element for pagination wrapper
+		paginationPoint: 'div.jcider-pagination-point' // Element for pagination points
 	});
 })
