21910020f93f3d01e3ab608d9458d6b64c93386d
caloloc2 <calolomino@gmail.com>
2018-06-24 18:17:21 -0500

    finalizado

--- gestion/includes/config.php
+++ gestion/includes/config.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Provee las constantes para conectarse a la base de datos
- * PostgreSQL.
- */
-
 define("HOSTNAME", "localhost");// Nombre del host (localhost aws instance)
 define("DATABASE", "web_edison"); // Nombre de la base de datos
 define("USERNAME", "root"); // Nombre del usuario
-define("PASSWORD", "385402292Mica_02"); // Contraseña
-
-
+define("PASSWORD", ""); // Contraseña
 ?>
